cc83802e3d1756482dd14a02af8a89447f2031bb
Mikhail Lazarev <mikael.lazarev@gmail.com>
2020-02-12 22:35:03 +0300

    prepare for heroku deployment

--- src/config.js
+++ src/config.js
@@ -9,4 +9,4 @@ export const ETH_MODERATOR_ADDRESS =
   '0xA6aA18aE16E000787063A404a44Da3A2AC26aa1c';
 export const APP_SPACE_NAME = 'FOAM_GUIDES';
 export const MAP_ACCESS_TOKEN =
-  'pk.eyJ1IjoibWlrYWVsbGF6YXJldiIsImEiOiJjazZhaTE0cTgwODJxM250aWxvejRmN2h5In0.kCyQNOkMpLgspjrtJQrU5A';
+  'pk.eyJ1IjoibWlrYWVsbGF6YXJldiIsImEiOiJjazZqcG5oOHMwMDNjM2tsZnB0eXpqdm1pIn0.RrmTOAWJhfjAjaVqZOW02A';
